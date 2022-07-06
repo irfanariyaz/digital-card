e07aca3fb838d3a6f46ebeea84b51251efdedf6c
Your Name <irfana.27@gmail.com>
2022-07-06 01:23:29 -0700

    updated header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,10 @@ export default function Header(){
            <h1>Irfana Sulaiman</h1>
            <h4>Frontend Developer</h4>
            <p>irfana.27@gmail.com</p>
+           <div>
+            <button></button>
+            <button></button>
+           </div>
         </header>
     )
 }
